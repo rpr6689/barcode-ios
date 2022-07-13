983ed0c75fbc7fdd883fb2e578b5bdb9914e8fc1
Scott Lukse <scott.lukse@hcahealthcare.com>
2022-07-13 09:40:56 -0400

    bar code scan first pass

--- BarcodeSender/BarcodeSender/ViewController.swift
+++ BarcodeSender/BarcodeSender/ViewController.swift
@@ -16,8 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var scanButton: UIButton!
     @IBOutlet weak var disconnectButton: UIButton!
     
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     var connection: NWConnection?
-    //var hostUDP: NWEndpoint.Host = "localhost"
     var hostUDP: NWEndpoint.Host = "10.0.0.5"
     var portUDP: NWEndpoint.Port = 4445
 
@@ -40,10 +42,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onScanClick(_ sender: Any) {
-        //TODO: handle scan here
-        
-        //TODO: after scan
-        //self.sendUDP(messageToUDP)
+        requestScanner()
     }
     
     @IBAction func onDisconnectClick(_ sender: Any) {
@@ -96,6 +95,96 @@ class ViewController: UIViewController {
             }
         })))
     }
-
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
 
+extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func requestScanner() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized: // The user has previously granted access to the camera.
+                self.initializeScanner()
+            
+            case .notDetermined: // The user has not yet been asked for camera access.
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        self.initializeScanner()
+                    }
+                }
+            
+            case .denied: // The user has previously denied access.
+                return
+
+            case .restricted: // The user can't grant access due to restrictions.
+                return
+            @unknown default:
+                return
+        }
+    }
+    
+    func initializeScanner() {
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        self.sendUDP(code)
+    }
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+}
